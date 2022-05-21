041634873fae53a49523f70de40f5edb94bb7227
Luis Caballero <lacb0311@hotmail.com>
2022-05-20 22:59:58 -0500

    matriz de confusion

--- diabetes.py
+++ diabetes.py
@@ -9,6 +9,10 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 url = 'diabetes.csv'
 data1 = pd.read_csv(url)
@@ -169,3 +173,25 @@ print(f'accuracy de Test de Entrenamiento: {np.array(acc_scores_test_train).mean
 
 # Accuracy de Validación
 print(f'accuracy de Validación: {rf.score(x_test_out, y_test_out)}')
+
+# Matriz de confusión
+print('*'*50)
+print(f'Matriz de confusión: {confusion_matrix(y_test_out, y_pred)}')
+
+matriz_confusion = confusion_matrix(y_test_out, y_pred)
+plt.figure(figsize = (6, 6))
+sns.heatmap(matriz_confusion)
+plt.title("Matriz de confusión")
+
+# Metricas
+precision = precision_score(y_test_out, y_pred, average=None).mean()
+print(f'Precisión: {precision}')
+
+recall = recall_score(y_test_out, y_pred, average=None).mean()
+print(f'Re-call: {recall}')
+
+f1 = f1_score(y_test_out, y_pred, average=None).mean()
+print(f'f1: {f1}')
+
+print(f'y real: {y_test_out}')
+print(f'y predicho: {y_pred}')
